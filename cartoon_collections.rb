302c5d87201d2cb4ad701d046d8a9ff21310bf62
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 01:51:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,8 @@ def long_planeteer_calls(array)
   }
 end
 
-def find_the_cheese ()
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if cheese_types.includes? array()
-    return array.find
-  
+ array.find do |cheese|
+    cheese_types.include?(cheese)
 end
