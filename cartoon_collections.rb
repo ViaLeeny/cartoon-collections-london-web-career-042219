51d21e35f24a78b85aed54272194c2a9af56bb22
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 00:59:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,9 @@ def roll_call_dwarves(array)
   end 
 end
 
-def summon_captain_planet (new_array)
-  new_array.collect { |word| word.capitalize +"!" }
+def summon_captain_planet (array)
+  array.collect { |word| word.capitalize +"!" }
+  return array
 end
 
 def long_planeteer_calls()
