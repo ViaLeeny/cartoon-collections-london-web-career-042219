29cbcaee5a604c5800cdfb864e1818e4b8183ef5
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 01:41:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find {
-    | |
-    
+    |cheese|
+    cheese == "cheadar" || cheese == "gouda" || cheese == "camembert"
   }
 end
