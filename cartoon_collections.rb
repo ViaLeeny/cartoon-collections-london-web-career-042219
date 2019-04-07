8953308d77d571506713828528ca928cd61efb6f
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 01:29:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,14 +5,16 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet (array)
-  
   array.collect do |word|
   word.capitalize + "!"
   end
 end
 
-def long_planeteer_calls()
-  
+def long_planeteer_calls(array)
+  array.any? { 
+  |string |
+    
+  }
 end
 
 def find_the_cheese ()
